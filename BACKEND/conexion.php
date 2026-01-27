daee7d624aba64f76b3681081a0ac673177635ba
iker-cuadra <icuadra@campuscamarafp.com>
2026-01-27 10:57:09 +0100

    jj

--- BACKEND/conexion.php
+++ BACKEND/conexion.php
@@ -1,13 +1,13 @@
 <?php
-$host = "localhost";
-$usuario = "root";         // Ajusta según tu config
-$contrasena = "";          // Ajusta según tu config
-$base_datos = "login_vinyl";
-$puerto = 3307;            // ⚠️ Reemplaza con tu puerto real
+$host = getenv('MYSQLHOST');
+$usuario = getenv('MYSQLUSER');
+$contrasena = getenv('MYSQLPASSWORD');
+$base_datos = getenv('MYSQLDATABASE');
+
 
 $conn = new mysqli($host, $usuario, $contrasena, $base_datos, $puerto);
 
-// Verificar conexión
+
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
 }
